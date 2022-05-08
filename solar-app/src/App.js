ff1f7f95ac08e088d1c65fe5cd7a17ba05291253
Richmond Tran <richmondtran8@gmail.com>
2022-05-07 19:13:53 -0600

    added figures

--- solar-app/src/App.js
+++ solar-app/src/App.js
@@ -3,12 +3,17 @@ import './App.css';
 import Splash from './components/Splash/Splash';
 import ProblemStatement from './components/ProblemStatement/ProblemStatement';
 
+import plot from './assets/figure/myplot.png';
+import photo_pot from './assets/figure/Photovoltaic Potential.png';
+
 function App() {
 
   return (
     <div className="App">
       <Splash />
       <ProblemStatement />
+      <img src={plot} alt='figure' className='figure' />
+      <img src={photo_pot} alt='photovoltaic potential' className='figure' />
     </div>
   );
 }
